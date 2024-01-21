51d0b3da9e00bbfb8d0a55a2a5d212d84ba58876
Yannick Cornaille <yannick.cornaille@orange.com>
2024-01-21 18:55:41 +0100

    feat: add GET /api/search/pokemons route to return the searched pokemons records

---
+++ pages/api/search/pokemons.ts
@@ -0,0 +1,19 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import clientPromise from '../../../lib/mongodb';
+
+export default async function(request:NextApiRequest, response:NextApiResponse) {
+  try {
+    const mongoClient = await clientPromise;
+    const db = mongoClient.db('pokemon');
+    const collection = db.collection('pokemons');
+    const results = await collection
+      .find({ name: { $regex: request.query.q, $options: 'i' } })
+      .project({ _id: 0, id: 1, apiTypes: 1, name: 1, image: 1, selected: 1})
+      .limit(15)
+      .toArray()
+    return response.status(200).json(results);
+  } catch (error) {
+    console.error(error);
+    return response.status(500).json({ error });
+  }
+}
